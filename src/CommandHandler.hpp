e8280c35cbbb10cc55f47fe5ea2a1ddf6ced79fb
Oleksandr Grytsov <al1img@users.noreply.github.com>
2017-08-08 14:09:04 +0300

    Initial commit
    * Initial commit
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Use map for command table
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to the latest sndif
    
    Signed-off-by: Oleksandr Andrushchenko <Oleksandr_Andrushchenko@epam.com>
    
    * Use system sndif header
    
    Signed-off-by: Oleksandr Andrushchenko <Oleksandr_Andrushchenko@epam.com>
    
    * Update to latest libxenbe
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Refactoring to use TypePtr instead of shared_ptr<Type>
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to sndif protocol v18
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to sndif protocol v18
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Format changes in getBufferRefs
    
    Signed-off-by: al1 <al1img@gmail.com>
    
    * Change instance id to device id
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to latest libxenbe
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to latest libxenbe
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add SoundItf
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add PulsePcm device
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Change cmake files: add compiling options
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Fix warnings
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add Pulse Async API
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * AlsaPcm: Use "default" device when device is empty
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * PulsePcm: add device parameter
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add config
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update README.md
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>

---
+++ src/CommandHandler.hpp
@@ -0,0 +1,79 @@
+/*
+ *  Command handler
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * Copyright (C) 2016 EPAM Systems Inc.
+ */
+
+#ifndef SRC_COMMANDHANDLER_HPP_
+#define SRC_COMMANDHANDLER_HPP_
+
+#include <cstdint>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+#include <xen/be/XenGnttab.hpp>
+#include <xen/be/Log.hpp>
+
+#include <xen/io/sndif.h>
+
+#include "SoundItf.hpp"
+
+/**
+ * Handles commands received from the frontend.
+ * @ingroup snd_be
+ */
+class CommandHandler
+{
+public:
+
+	/**
+	 * @param type  Alsa stream type
+	 * @param domId domain id
+	 */
+	CommandHandler(std::shared_ptr<SoundItf::PcmDevice> pcmDevice,
+				   SoundItf::StreamType type, int domId);
+	~CommandHandler();
+
+	/**
+	 * Processes commands received from the frontend.
+	 * @param req
+	 * @return status
+	 */
+	int processCommand(const xensnd_req& req);
+
+private:
+
+	typedef void(CommandHandler::*CommandFn)(const xensnd_req& req);
+
+	static std::unordered_map<int, CommandFn> sCmdTable;
+
+	std::shared_ptr<SoundItf::PcmDevice> mPcmDevice;
+	int mDomId;
+	std::unique_ptr<XenBackend::XenGnttabBuffer> mBuffer;
+
+	XenBackend::Log mLog;
+
+	void open(const xensnd_req& req);
+	void close(const xensnd_req& req);
+	void read(const xensnd_req& req);
+	void write(const xensnd_req& req);
+
+	void getBufferRefs(grant_ref_t startDirectory, uint32_t size, std::vector<grant_ref_t>& refs);
+};
+
+#endif /* SRC_COMMANDHANDLER_HPP_ */
---
+++ src/SndBackend.hpp
@@ -0,0 +1,155 @@
+/*
+ *  Sound backend
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * Copyright (C) 2016 EPAM Systems Inc.
+ */
+
+#ifndef SRC_SNDBACKEND_HPP_
+#define SRC_SNDBACKEND_HPP_
+
+#include <xen/be/BackendBase.hpp>
+#include <xen/be/FrontendHandlerBase.hpp>
+#include <xen/be/RingBufferBase.hpp>
+#include <xen/be/Log.hpp>
+
+#include "CommandHandler.hpp"
+#include "Config.hpp"
+
+#ifdef WITH_ALSA
+#include "AlsaPcm.hpp"
+#endif
+
+#ifdef WITH_PULSE
+#include "PulsePcm.hpp"
+#endif
+
+/***************************************************************************//**
+ * @defgroup snd_be
+ * Backend related classes.
+ ******************************************************************************/
+
+/***************************************************************************//**
+ * Ring buffer used for the audio stream.
+ * @ingroup snd_be
+ ******************************************************************************/
+class StreamRingBuffer : public XenBackend::RingBufferInBase<
+											xen_sndif_back_ring,
+											xen_sndif_sring,
+											xensnd_req,
+											xensnd_resp>
+{
+public:
+	/**
+	 * @param id    stream id
+	 * @param type  stream type
+	 * @param domId frontend domain id
+	 * @param port  event channel port number
+	 * @param ref   grant table reference
+	 */
+	StreamRingBuffer(int id, std::shared_ptr<SoundItf::PcmDevice> pcmDevice,
+					 SoundItf::StreamType streamType, domid_t domId,
+					 evtchn_port_t port, grant_ref_t ref);
+
+private:
+	int mId;
+	CommandHandler mCommandHandler;
+	XenBackend::Log mLog;
+
+	void processRequest(const xensnd_req& req);
+};
+
+/***************************************************************************//**
+ * Sound frontend handler.
+ * @ingroup snd_be
+ ******************************************************************************/
+class SndFrontendHandler : public XenBackend::FrontendHandlerBase
+{
+public:
+
+	/**
+	 * @param domId   frontend domain id
+	 * @param backend backend instance
+	 * @param id      frontend instance id
+	 */
+	SndFrontendHandler(Config& config, const std::string devName,
+					   domid_t beDomId, domid_t feDomId, uint16_t devId) :
+		FrontendHandlerBase("SndFrontend", devName, beDomId, feDomId, devId),
+#ifdef WITH_PULSE
+		mPulseMainloop(getDomName()),
+#endif
+		mConfig(config),
+		mLog("SndFrontend") {}
+
+protected:
+
+	/**
+	 * Is called on connected state when ring buffers binding is required.
+	 */
+	void onBind() override;
+
+	/**
+	 * Is called on connected state when ring buffers releases are required.
+	 */
+	void onClosing() override;
+
+private:
+
+#ifdef WITH_PULSE
+	Pulse::PulseMainloop mPulseMainloop;
+#endif
+
+	Config& mConfig;
+
+	XenBackend::Log mLog;
+
+	std::shared_ptr<SoundItf::PcmDevice> createPcmDevice(
+			SoundItf::StreamType type, int id);
+	void createStream(int id, SoundItf::StreamType type,
+					  const std::string& streamPath);
+	void processCard(const std::string& cardPath);
+	void processDevice(const std::string& devPath);
+	void processStream(const std::string& streamPath);
+};
+
+/***************************************************************************//**
+ * Sound backend class.
+ * @ingroup snd_be
+ ******************************************************************************/
+class SndBackend : public XenBackend::BackendBase
+{
+public:
+
+	SndBackend(Config& config, const std::string& deviceName,
+			   domid_t domId) : BackendBase("SndBackend", deviceName, domId),
+		mConfig(config)
+	{}
+
+protected:
+
+	/**
+	 * Is called when new sound frontend appears.
+	 * @param domId domain id
+	 * @param devId device id
+	 */
+	void onNewFrontend(domid_t domId, uint16_t devId) override;
+
+private:
+
+	Config& mConfig;
+};
+
+#endif /* SRC_SNDBACKEND_HPP_ */
