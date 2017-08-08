052c06645d2e1f7b9b185ede3845bc639af80ca0
Oleksandr Grytsov <al1img@users.noreply.github.com>
2017-08-08 13:55:46 +0300

    Initial commit
    * Initial commit
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Use map for command table
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to the latest sndif
    
    Signed-off-by: Oleksandr Andrushchenko <Oleksandr_Andrushchenko@epam.com>
    
    * Use system sndif header
    
    Signed-off-by: Oleksandr Andrushchenko <Oleksandr_Andrushchenko@epam.com>
    
    * Update to latest libxenbe
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Refactoring to use TypePtr instead of shared_ptr<Type>
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to sndif protocol v18
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to sndif protocol v18
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Format changes in getBufferRefs
    
    Signed-off-by: al1 <al1img@gmail.com>
    
    * Change instance id to device id
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to latest libxenbe
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update to latest libxenbe
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add SoundItf
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add PulsePcm device
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Change cmake files: add compiling options
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Fix warnings
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add Pulse Async API
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * AlsaPcm: Use "default" device when device is empty
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * PulsePcm: add device parameter
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Add config
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>
    
    * Update README.md
    
    Signed-off-by: Oleksandr Grytsov <al1img@gmail.com>

---
+++ src/AlsaPcm.hpp
@@ -0,0 +1,97 @@
+/*
+ *  Alsa pcm device wrapper
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * Copyright (C) 2016 EPAM Systems Inc.
+ */
+
+#ifndef SRC_ALSAPCM_HPP_
+#define SRC_ALSAPCM_HPP_
+
+#include <alsa/asoundlib.h>
+
+#include <xen/be/Log.hpp>
+
+#include "SoundItf.hpp"
+
+namespace Alsa {
+
+/***************************************************************************//**
+ * @defgroup alsa
+ * Alsa related classes.
+ ******************************************************************************/
+
+/***************************************************************************//**
+ * Provides alsa pcm functionality.
+ * @ingroup alsa
+ ******************************************************************************/
+class AlsaPcm : public SoundItf::PcmDevice
+{
+public:
+	/**
+	 * @param type stream type
+	 * @param name pcm device name
+	 */
+	explicit AlsaPcm(SoundItf::StreamType type,
+					 const std::string& deviceName = "default");
+	~AlsaPcm();
+
+	/**
+	 * Opens the pcm device.
+	 * @param params pcm parameters
+	 */
+	void open(const SoundItf::PcmParams& params) override;
+
+	/**
+	 * Closes the pcm device.
+	 */
+	void close() override;
+
+	/**
+	 * Reads data from the pcm device.
+	 * @param buffer buffer where to put data
+	 * @param size   number of bytes to read
+	 */
+	void read(uint8_t* buffer, size_t size) override;
+
+	/**
+	 * Writes data to the pcm device.
+	 * @param buffer buffer with data
+	 * @param size   number of bytes to write
+	 */
+	void write(uint8_t* buffer, size_t size) override;
+
+private:
+
+	struct PcmFormat
+	{
+		uint8_t sndif;
+		snd_pcm_format_t alsa;
+	};
+
+	static PcmFormat sPcmFormat[];
+
+	snd_pcm_t *mHandle;
+	std::string mDeviceName;
+	SoundItf::StreamType mType;
+	XenBackend::Log mLog;
+
+	snd_pcm_format_t convertPcmFormat(uint8_t format);
+};
+
+}
+
+#endif /* SRC_ALSAPCM_HPP_ */
